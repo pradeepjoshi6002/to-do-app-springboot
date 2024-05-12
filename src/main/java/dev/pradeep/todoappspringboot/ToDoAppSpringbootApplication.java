218c07916471cb88372f33250182df77f9b64710
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2024-05-12 20:28:47 +0530

    intialized dependencies

---
+++ src/main/java/dev/pradeep/todoappspringboot/ToDoAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.pradeep.todoappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/pradeep/todoappspringboot/ToDoAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pradeep.todoappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
